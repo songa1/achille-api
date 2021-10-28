306ebbe63d417c51c6d3c14f85ec1df2b87695b7
songa1 <songachillethe1her@gmail.com>
2021-10-28 13:36:09 +0200

    I have connected a database"

--- db/connect.js
+++ db/connect.js
@@ -1,15 +1,12 @@
-const mongoose = require("mongoose");
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-const username = process.env.MONGO_USER;
-const password = process.env.MONGO_PASSWORD;
-const cluster = process.env.MONGO_CLUSTER;
-const dbname = process.env.MONGO_DB;
 
-await mongoose.connect(
-    `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`, 
-    {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
-    }
-);
+mongoose.connect(process.env.NODE_ENV==='test' ?  process.env.DBTEST : process.env.DB, { useNewUrlParser: true , useUnifiedTopology: true, })
+.then(function(){
+    console.log('db connected');
+}).catch((err)=>{
+    console.log(err.message);
+});
+
+mongoose.Promise = global.Promise;
--- index.js
+++ index.js
@@ -1,28 +1,16 @@
 const express = require('express');
-const http = require('http');
-const Router = require("../router/router.js")
-require('dotenv').config()
+const route = require("./router/router")
+require('dotenv').config();
+require('./db/connect');
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/achille/api", Router);
-
-mongoose.connect(
-    `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`, 
-    {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
-    }
-  );
-
-  db.on("error", console.error.bind(console, "connection error: "));
-  db.once("open", function () {
-    console.log("Connected successfully");
-  });
-
-app.listen('5000', ()=>{
-  console.log("App running on port 5000");
+app.listen(process.env.PORT || 2701, ()=>{
+  console.log('Now listening to requests!');
 });
+
+app.use("/achille/api", route);
+
+module.exports = app;
